fb11ca5c5e097330b62be104043ef3d27034514c
Toomore Chiang <toomore0929@gmail.com>
2015-07-02 15:29:02 +0800

    Make more simple.

--- s3/s3.go
+++ s3/s3.go
@@ -8,25 +8,33 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func NewPutObject(Bucket, Key string, Body io.ReadSeeker) *s3.PutObjectInput {
-	return &s3.PutObjectInput{
-		Bucket: aws.String(Bucket),
+type S3 struct {
+	bucket   *string
+	s3bucket *s3.S3
+}
+
+func (s S3) Get(Key string) (*s3.GetObjectOutput, error) {
+	return s.s3bucket.GetObject(&s3.GetObjectInput{
+		Bucket: s.bucket,
+		Key:    aws.String(Key),
+	})
+}
+
+func (s S3) Put(Key string, Body io.ReadSeeker) (*s3.PutObjectOutput, error) {
+	return s.s3bucket.PutObject(&s3.PutObjectInput{
+		Bucket: s.bucket,
 		Key:    aws.String(Key),
 		Body:   Body,
-	}
+	})
 }
 
-func NewGetObject(Bucket, Key string) *s3.GetObjectInput {
-	return &s3.GetObjectInput{
-		Bucket: aws.String(Bucket),
-		Key:    aws.String(Key),
-	}
-}
-
-// New to new a ses.SES
-func New(AWSID, AWSKEY, Region string) *s3.S3 {
+// New to new a ses.S3
+func New(AWSID, AWSKEY, Region, Bucket string) *S3 {
 	var config = aws.DefaultConfig
 	config.Region = Region
 	config.Credentials = credentials.NewStaticCredentials(AWSID, AWSKEY, "")
-	return s3.New(config)
+	return &S3{
+		bucket:   aws.String(Bucket),
+		s3bucket: s3.New(config),
+	}
 }
--- s3/s3_test.go
+++ s3/s3_test.go
@@ -10,17 +10,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awsutil"
 )
 
-var s3bucket = New(os.Getenv("AWSID"), os.Getenv("AWSKEY"), "us-east-1")
+var s3bucket = New(os.Getenv("AWSID"), os.Getenv("AWSKEY"), "us-east-1", "toomore-aet")
 
 func TestNewPutObject(t *testing.T) {
-	var object = NewPutObject("toomore-aet", "simples3-test.txt", bytes.NewReader([]byte("This is test for simples3.")))
-	resp, _ := s3bucket.PutObject(object)
+	resp, _ := s3bucket.Put("simples3-test.txt", bytes.NewReader([]byte("This is test for simples3.")))
 	fmt.Println(awsutil.StringValue(resp))
 }
 
 func TestNewGetObject(t *testing.T) {
-	var object = NewGetObject("toomore-aet", "simples3-test.txt")
-	resp, _ := s3bucket.GetObject(object)
+	resp, _ := s3bucket.Get("simples3-test.txt")
 	fmt.Println(awsutil.StringValue(resp))
 	if body, err := ioutil.ReadAll(resp.Body); err == nil {
 		fmt.Printf("[%s]\n", body)
